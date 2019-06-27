72780281aa10c7d57f4cc5697df561fe23f3a7ab
bruno-reis <13205396+bruno-reis@users.noreply.github.com>
2019-06-27 17:07:36 +0200

    Added Chapter 1 code

---
+++ ch1/src/test/kotlin/taco/cloud/app/HomeControllerTest.kt
@@ -0,0 +1,30 @@
+package taco.cloud.app
+
+import org.hamcrest.Matchers.containsString
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(HomeController::class)
+class HomeControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    @Throws(Exception::class)
+    fun testHomePage() {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk)
+            .andExpect(view().name("Home"))
+            .andExpect(content().string(containsString("Welcome to...")))
+    }
+}
